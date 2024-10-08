be6a08de16f045c5992567a4c739fb60b4faa048
Ayman Ougri <waougri@gmail.com>
2024-10-07 21:06:01 -0400

    QoL changes, added documentation.

--- functionParser/Tokenizer.hpp
+++ functionParser/Tokenizer.hpp
@@ -9,300 +9,276 @@
 #include <iostream>
 #include <queue>
 #include <sstream>
+#include <stack>
 #include <string_view>
 #include <termcolor/termcolor.hpp>
 #include <variant>
 #include <vector>
-#include <stack>
-
-namespace Tokenizer {
-enum class Operator : char {
-  Sum = '+',
-  Sub = '-',
-  Div = '/',
-  Pow = '^',
-  Mult = '*',
-  LParen = '(',
-  RParen = ')',
-  Comma = ',',
-  None = '\0'
-};
-bool isOperator(const Operator c) {
-  switch (c) {
-    case Operator::Sum:
-    case Operator::Sub:
-    case Operator::Div:
-    case Operator::Mult:
-    case Operator::Pow:
-    case Operator::LParen:
-    case Operator::RParen:
-    case Operator::Comma:
-      return true;
-    case Operator::None:
-    default:
-      return false;
-  }
-}
-
-std::ostream& operator<<(std::ostream& out, const Operator& op) {
-  out << static_cast<char>(op);
-  return out;
-}
-
-struct UnaryFunction {
-  std::string name{};
-  std::function<double(double)> fn{};
-};
-std::ostream& operator<<(std::ostream& out, const UnaryFunction& func) {
-  out << func.name;
-  return out;
-}
-struct BinaryFunction {
-  std::string name;
-  std::function<double(double, double)> fn{};
-};
-
-std::unordered_map<Operator, BinaryFunction> op_to_fn = {
-    {Operator::Sum,
-     BinaryFunction{"Sum", [](double a, double b) { return a + b; }}},
-    {Operator::Sub,
-     BinaryFunction{"Sum", [](double a, double b) { return a - b; }}},
-    {Operator::Div, BinaryFunction{"Sum",
-                                   [](double a, double b) {
-                                     assert(b != 0);
-                                     return a / b;
-                                   }}},
-    {Operator::Mult,
-     BinaryFunction{"Sum", [](double a, double b) { return a * b; }}},
-    {Operator::Pow,
-     BinaryFunction{"Sum", [](double a, double b) { return std::pow(a, b); }}},
-
-};
-
-enum class Associativity {
-  Left,
-  Right,
-  None,
-};
-struct OperatorInfo {
-  int precedence{};
-  Associativity associativity{};
-};
-std::unordered_map<Operator, OperatorInfo> operator_info{
-    {Operator::Pow, {4, Associativity::Right}},
-    {Operator::Mult, {3, Associativity::Left}},
-    {Operator::Div, {3, Associativity::Left}},
-    {Operator::Sub, {2, Associativity::Left}},
-    {Operator::Sum, {2, Associativity::Left}},
-};
-
-std::ostream& operator<<(std::ostream& out, const BinaryFunction& func) {
-  out << func.name;
-  return out;
-}
-
-struct Variable {
-  char name{};
-  double value{};
-};
-std::ostream& operator<<(std::ostream& out, const Variable& var) {
-  out << fmt::format("[{} = {}]", var.name, var.value);
-
-  return out;
-}
-
-using TokenType =
-    std::variant<double, Variable, Operator, UnaryFunction, BinaryFunction>;
-
-void print_token(const TokenType& token) {
-  std::visit(
-      [](const auto& value) {
-        std::cout << value << ", ";  // Print the value
-      },
-      token);
-}
-
-void print_container(const std::vector<TokenType>& cont) {
-  for (const auto& item : cont) {
-    print_token(item);
-  }
-  std::cout << std::endl;
-}
-auto tokenize(const std::string_view expression) -> std::vector<TokenType> {
-  std::cout << expression << '\n';
-  decltype(tokenize(expression)) vec{};
-  char curr = 0;
-  std::ostringstream oss{};
-  for (int pos = 0; pos < std::size(expression); ++pos) {
-    curr = expression.at(pos);
-    Logger::log(fmt::format("current char: expr[{}] = {} \n", pos, curr));
-    if (isdigit(curr) || curr == '.') {
-      oss << curr;
-    } else {
-      // stopped getting digits in
-      if (!oss.str().empty()) {
-        // convert the digits acc'ed into a double
-        const double res = std::stod(oss.str());
-        // check if the double is valid
-        if (not std::isnan(res)) {
-          // push the number, clear the buffer.
-          Logger::log(fmt::format("Token (number = {}) has been pushed", res),
-                      Logger::LogLevel::kInfo);
-          vec.emplace_back(res);
-          oss = {};
-        }
-      }
-      if (isOperator(static_cast<Operator>(curr))) {
-        Logger::log(fmt::format("Token (Operator = {}) has been pushed", curr),
-                    Logger::LogLevel::kInfo);
-        vec.emplace_back(static_cast<Operator>(curr));
-        oss = {};
-      }
-    }
-  }
-  // Recheck for the last char.
-  if (!oss.str().empty()) {
-    // convert the digits acc'ed into a double
-    const double res = std::stod(oss.str());
-    // check if the double is valid
-    if (not std::isnan(res)) {
-      // push the number, clear the buffer.
-      Logger::log(fmt::format("Token (number = {}) has been pushed", res),
-                  Logger::LogLevel::kInfo);
-      vec.emplace_back(res);
-      oss = {};
-    }
-  }
-
-  print_container(vec);
-  return vec;
-}
-
-bool isNumber(const TokenType& token) {
-  return std::holds_alternative<double>(token);
-}
-bool isOperator(const TokenType& token) {
-  return std::holds_alternative<Operator>(token);
-}
-bool isFunction(const TokenType& token) { return false; }
-
-std::ostream& operator<<(std::ostream& out, const std::queue<TokenType>& q) {
-  auto cp = q;
-
-  while (not cp.empty()) {
-    print_token(cp.front());
-    cp.pop();
-  }
-
-  return out;
-}
-
-// Helper function to convert a TokenType to a string
-inline std::string tokenToString(const TokenType& token) {
-  if (std::holds_alternative<double>(token)) {
-    return std::to_string(std::get<double>(token));
-  } else if (std::holds_alternative<Operator>(token)) {
-    return {static_cast<char>(std::get<Operator>(token))};
-  }
-  return "";
-}
-
-// Function to convert the entire queue to a string
-inline std::string queueToString(const std::queue<TokenType>& queue) {
-  std::ostringstream oss;
-  std::queue<TokenType> temp_queue =
-      queue;  // Create a copy to preserve the original queue
-
-  while (!temp_queue.empty()) {
-    oss << tokenToString(temp_queue.front());
-    temp_queue.pop();
-    if (!temp_queue.empty()) {
-      oss << " ";  // Add space between tokens
-    }
-  }
-
-  return oss.str();
-}
-bool isOperatorButNotAParen(const Operator op) {
-  return op != Operator::LParen and op != Operator::RParen;
-}
-bool isAParen(const Operator op) {
-  return op == Operator::LParen or op == Operator::RParen;
-}
-
-inline int getOperatorPrecedence(const Operator& op) {
-  if (op == Operator::LParen || op == Operator::RParen) {
-    return -1;
-  }
-  return operator_info[op].precedence;
-}
-inline Associativity getAssociativity(const Operator& op) {
-  if (op == Operator::LParen || op == Operator::RParen) {
-    return Associativity::None;
-  }
-  return operator_info[op].associativity;
-}
 
 /**
- * @brief Shunting yard algo to transform a classic math expression into RPN. Taken from https://en.wikipedia.org/wiki/Shunting_yard_algorithm
- * @anchor https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+ * @namespace Tokenizer
+ * @brief A namespace for parsing and evaluating mathematical expressions.
+ *
+ * This namespace provides functionality for tokenizing expressions, transforming them
+ * to Reverse Polish Notation (RPN) using the Shunting Yard algorithm, and evaluating
+ * mathematical expressions. It includes structures for operators, functions, variables,
+ * and utilities for handling tokens.
  */
-inline std::vector<TokenType> shunting_yard(const std::string& expression) {
-  using namespace Logger;
-  using Logger::LogLevel;
+namespace Tokenizer {
 
-  std::vector<TokenType> tokens = tokenize(expression);
-  std::vector<TokenType> output_queue{};
-  std::stack<Operator> op_stack{};
-  std::ostringstream oss{};
-  for (const auto& token : tokens) {
-    if (isNumber(token)) {
-      output_queue.emplace_back(token);
-      oss << std::get<double>(token);
-    } else if (isOperator(token)) {
-      auto op = std::get<Operator>(token);
-      if(isOperatorButNotAParen(op)) {
-      auto o1 = std::get<Operator>(token);
-      Logger::log(fmt::format("Token (Operator = {}) is found.", static_cast<char>(o1)));
-      while ((not op_stack.empty() &&
-              op_stack.top() != Operator::LParen) and
-             ((getOperatorPrecedence(op_stack.top()) >
-               getOperatorPrecedence(o1)) or
-              ((getOperatorPrecedence(op_stack.top()) ==
-                getOperatorPrecedence(o1)) and
-               getAssociativity(o1) == Associativity::Left))) {
-        output_queue.emplace_back(op_stack.top());
-        oss << static_cast<char>(op_stack.top());
-        op_stack.pop();
-      }
-      op_stack.push(o1);
-        continue;
-      } else if (isAParen(op)) {
-        if(op == Operator::LParen) {
-          op_stack.emplace(op);
-          continue;
-        } else if (op == Operator::RParen) {
-            while(not op_stack.empty() and op_stack.top() != Operator::LParen) {
-        output_queue.emplace_back(op_stack.top());
-        oss << static_cast<char>(op_stack.top());
-        op_stack.pop();
+/**
+ * @enum Operator
+ * @brief Enum class representing various mathematical operators.
+ *
+ * This enum defines the character representations for different operators used in
+ * mathematical expressions.
+ */
+enum class Operator : char {
+    Sum = '+', ///< Addition operator
+    Sub = '-', ///< Subtraction operator
+    Div = '/', ///< Division operator
+    Pow = '^', ///< Exponentiation operator
+    Mult = '*', ///< Multiplication operator
+    LParen = '(', ///< Left parenthesis
+    RParen = ')', ///< Right parenthesis
+    Comma = ',', ///< Comma (for separating function arguments)
+    None = '\0' ///< No operator
+};
 
-        }
-        assert(op_stack.top() == Operator::LParen);
-        op_stack.pop();
+/**
+ * @struct UnaryFunction
+ * @brief Represents a unary mathematical function.
+ *
+ * This structure captures the name of the function and a callable that performs
+ * the operation on a single double value.
+ */
+struct UnaryFunction {
+    std::string name {}; ///< Name of the unary function
+    std::function<double(double)> fn {}; ///< Function to apply
+};
 
-        }
-      }
+/**
+ * @struct BinaryFunction
+ * @brief Represents a binary mathematical function.
+ *
+ * This structure includes the name of the function and a callable that performs
+ * the operation on two double values.
+ */
+struct BinaryFunction {
+    std::string name; ///< Name of the binary function
+    std::function<double(double, double)> fn {}; ///< Function to apply
+};
 
-    }
-  }
+/**
+ * @enum Associativity
+ * @brief Enum class representing the associativity of operators.
+ */
+enum class Associativity {
+    Left, ///< Left associative
+    Right, ///< Right associative
+    None, ///< No associativity
+};
 
-    while(not op_stack.empty()) {
+/**
+ * @struct OperatorInfo
+ * @brief Contains information about an operator.
+ *
+ * This structure holds the precedence and associativity of a given operator.
+ */
+struct OperatorInfo {
+    int precedence {}; ///< Precedence level of the operator
+    Associativity associativity {}; ///< Associativity of the operator
+};
 
-        output_queue.emplace_back(op_stack.top());
-        oss << op_stack.top();
-        op_stack.pop();
-    }
-  return output_queue;
-};  // namespace Tokenizer
-}  // namespace Tokenizer
+/**
+ * @struct Variable
+ * @brief Represents a variable in an expression.
+ *
+ * This structure captures the name and value of a variable.
+ */
+struct Variable {
+    char name {}; ///< Name of the variable
+    double value {}; ///< Value of the variable
+};
+
+/**
+ * @typedef TokenType
+ * @brief A variant type representing different token types.
+ *
+ * This type can hold a double, a variable, an operator, a unary function, or a binary function.
+ */
+using TokenType = std::variant<double, Variable, Operator, UnaryFunction, BinaryFunction>;
+
+/**
+ * @brief Checks if the given character is a valid operator.
+ * @param c The character to check.
+ * @return true if the character is an operator, false otherwise.
+ */
+bool isOperator(const Operator c);
+
+/**
+ * @brief Overloads the output stream operator for Operator.
+ * @param out The output stream.
+ * @param op The operator to output.
+ * @return The modified output stream.
+ */
+std::ostream& operator<<(std::ostream& out, const Operator& op);
+
+/**
+ * @brief Overloads the output stream operator for UnaryFunction.
+ * @param out The output stream.
+ * @param func The unary function to output.
+ * @return The modified output stream.
+ */
+std::ostream& operator<<(std::ostream& out, const UnaryFunction& func);
+
+/**
+ * @brief Overloads the output stream operator for BinaryFunction.
+ * @param out The output stream.
+ * @param func The binary function to output.
+ * @return The modified output stream.
+ */
+std::ostream& operator<<(std::ostream& out, const BinaryFunction& func);
+
+/**
+ * @brief Overloads the output stream operator for Variable.
+ * @param out The output stream.
+ * @param var The variable to output.
+ * @return The modified output stream.
+ */
+std::ostream& operator<<(std::ostream& out, const Variable& var);
+
+/**
+ * @brief Overloads the output stream operator for a queue of TokenType.
+ * @param out The output stream.
+ * @param q The queue of tokens to output.
+ * @return The modified output stream.
+ */
+std::ostream& operator<<(std::ostream& out, const std::queue<TokenType>& q);
+
+/**
+ * @brief Maps operators to their corresponding binary functions.
+ */
+const inline std::unordered_map<Operator, BinaryFunction> op_to_fn = {
+    { Operator::Sum, BinaryFunction { "Sum", [](double a, double b) { return a + b; } } },
+    { Operator::Sub, BinaryFunction { "Sub", [](double a, double b) { return a - b; } } },
+    { Operator::Div, BinaryFunction { "Div", [](double a, double b) {
+                                         assert(b != 0);
+                                         return a / b;
+                                     } } },
+    { Operator::Mult, BinaryFunction { "Mult", [](double a, double b) { return a * b; } } },
+    { Operator::Pow, BinaryFunction { "Pow", [](double a, double b) { return std::pow(a, b); } } },
+};
+
+/**
+ * @brief Maps operators to their precedence and associativity information.
+ */
+const inline std::unordered_map<Operator, OperatorInfo> operator_info = {
+    { Operator::Pow, { 4, Associativity::Right } },
+    { Operator::Mult, { 3, Associativity::Left } },
+    { Operator::Div, { 3, Associativity::Left } },
+    { Operator::Sub, { 2, Associativity::Left } },
+    { Operator::Sum, { 2, Associativity::Left } },
+};
+
+/**
+ * @brief Prints a token to the console.
+ * @param token The token to print.
+ */
+inline void print_token(const TokenType& token);
+
+/**
+ * @brief Prints a container of tokens to the console.
+ * @param cont The vector of tokens to print.
+ */
+inline void print_container(const std::vector<TokenType>& cont);
+
+/**
+ * @brief Checks if the token is a number.
+ * @param token The token to check.
+ * @return true if the token is a number, false otherwise.
+ */
+bool isNumber(const TokenType& token);
+
+/**
+ * @brief Checks if the token is an operator.
+ * @param token The token to check.
+ * @return true if the token is an operator, false otherwise.
+ */
+bool isOperator(const TokenType& token);
+
+/**
+ * @brief Checks if the token represents a function.
+ * @param token The token to check.
+ * @return true if the token is a function, false otherwise.
+ */
+bool isFunction(const TokenType& token);
+
+/**
+ * @brief Checks if the operator is not a parenthesis.
+ * @param op The operator to check.
+ * @return true if the operator is not a parenthesis, false otherwise.
+ */
+bool isOperatorButNotAParen(const Operator op);
+
+/**
+ * @brief Checks if the token is a variable.
+ * @param tok The token to check.
+ * @return true if the token is a variable, false otherwise.
+ */
+bool isVariable(const TokenType& tok);
+
+/**
+ * @brief Checks if the operator is a parenthesis.
+ * @param op The operator to check.
+ * @return true if the operator is a parenthesis, false otherwise.
+ */
+bool isAParen(const Operator op);
+
+/**
+ * @brief Gets the precedence of an operator.
+ * @param op The operator whose precedence is to be determined.
+ * @return The precedence level of the operator.
+ */
+int getOperatorPrecedence(const Operator& op);
+
+/**
+ * @brief Gets the associativity of an operator.
+ * @param op The operator whose associativity is to be determined.
+ * @return The associativity of the operator.
+ */
+Associativity getAssociativity(const Operator& op);
+
+/**
+ * @brief Tokenizes the given mathematical expression.
+ * @param expression The expression to tokenize.
+ * @return A vector of TokenType representing the tokenized expression.
+ */
+auto tokenize(const std::string_view expression) -> std::vector<TokenType>;
+
+/**
+ * @brief Converts a TokenType to a string representation.
+ * @param token The token to convert.
+ * @return A string representation of the token.
+ */
+inline std::string tokenToString(const TokenType& token);
+
+/**
+ * @brief Implements the Shunting Yard algorithm to convert infix expressions to RPN.
+ *
+ * This algorithm was adapted from the Wikipedia page:
+ * https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+ *
+ * @param expression The infix expression to convert.
+ * @return A vector of TokenType representing the expression in RPN.
+ */
+std::vector<TokenType> shunting_yard(const std::string& expression);
+
+/**
+ * @brief Evaluates a mathematical expression given as a string.
+ * @param expression The expression to evaluate.
+ * @return The result of the evaluation as a double.
+ */
+double evaluate(const std::string& expression);
+
+} // namespace Tokenizer
--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,22 @@
+#include "Tokenizer.hpp"
 #include <fmt/core.h>
 
-#include <Tokenizer.hpp>
 #include <Types.hpp>
 #include <cmath>
 
-int main() {
-  //   types::Divide add{};
-  //   add.left = new types::ValueNode(2.0);
-  //   add.right = new types::ValueNode(0);
+int main()
+{
+    //   types::Divide add{};
+    //   add.left = new types::ValueNode(2.0);
+    //   add.right = new types::ValueNode(0);
 
-  //   add.print();                               // Print the expression
-  //   fmt::print("\nResult: {}\n", add.eval());  // Evaluate and print the
-  //   result
+    //   add.print();                               // Print the expression
+    //   fmt::print("\nResult: {}\n", add.eval());  // Evaluate and print the
+    //   result
 
-  //   // Clean up dynamically allocated memory
-  //   delete add.left;
-  //   delete add.right;
-  // Tokenizer::tokenize("0.23 + 25889.333");
+    //   // Clean up dynamically allocated memory
+    //   delete add.left;
+    //   delete add.right;
+    // Tokenizer::tokenize("0.23 + 25889.333");
+    fmt::println("{}", Tokenizer::evaluate("3 + 2 + 2"));
 }
